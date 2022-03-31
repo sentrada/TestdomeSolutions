7f65842e662963e0a8fdc37dc5c52c4db1503e36
Bulldozer 63 <sentrada@gmail.com>
2022-03-31 11:11:23 +0200

    Train Composition
    Signed-off-by: Fukszius Ottó <fuksziuso@nexon.hu>

---
+++ Net47/TrainComposition/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+namespace TrainComposition
+{
+    public class Wagon{
+        public int Id { get; }
+
+        public Wagon RightTrain { get; private set; }
+
+        public Wagon LeftTrain { get; private set; }
+
+        public Wagon(int id){
+            Id=id;
+            LeftTrain=null;
+            RightTrain=null;
+        }
+        public void SetRightTrain(Wagon right){
+            RightTrain=right;
+        }
+        public void SetLeftTrain(Wagon left){
+            LeftTrain=left;
+        }
+    }
+
+
+    public class TrainComposition
+    {
+        private Wagon _leftMost;
+        private Wagon _rightMost;
+
+        public TrainComposition()
+        {
+            _leftMost = null;
+            _rightMost = null;
+        }
+
+        public void AttachWagonFromLeft(int wagonId)
+        {
+            var node = new Wagon(wagonId);
+            if (_leftMost != null)
+            {
+                //trains in composition
+                _leftMost.SetLeftTrain(node);
+                node.SetRightTrain(_leftMost);
+                _leftMost = node;
+            }
+            else
+            {
+                SetNewTrain(node);
+            }
+        }
+
+        public void AttachWagonFromRight(int wagonId)
+        {
+            var node = new Wagon(wagonId);
+            if (_rightMost != null)
+            {
+                //trains in composition
+                _rightMost.SetRightTrain(node);
+                node.SetLeftTrain(_rightMost);
+                _rightMost = node;
+            }
+            else
+            {
+                SetNewTrain(node);
+            }
+        }
+
+        private void SetNewTrain(Wagon node)
+        {
+            _leftMost = node;
+            _rightMost = node;
+        }
+
+        public int DetachWagonFromLeft()
+        {
+            var wagonId = 0;
+            if (_leftMost != null)
+            {
+                wagonId = _leftMost.Id;
+                _leftMost = _leftMost.RightTrain;
+            }
+
+            return wagonId;
+        }
+
+        public int DetachWagonFromRight()
+        {
+            var wagonId = 0;
+            if (_rightMost != null)
+            {
+                wagonId = _rightMost.Id;
+                _rightMost = _rightMost.LeftTrain;
+            }
+
+            return wagonId;
+        }
+
+        public static void Main(string[] args)
+        {
+            TrainComposition train = new TrainComposition();
+            train.AttachWagonFromLeft(7);
+            train.AttachWagonFromLeft(13);
+            Console.WriteLine(train.DetachWagonFromRight()); // 7 
+            Console.WriteLine(train.DetachWagonFromLeft()); // 13
+        }
+    }
+}
--- README.md
+++ README.md
@@ -2,14 +2,15 @@
 https://www.testdome.com/d/c-sharp-interview-questions/18
 Testdome practice job interview questions solutions
 .Net47 - 
-1. Alert Service      - https://www.testdome.com/questions/c-sharp/alert-service/31293?visibility=1&skillId=18
-2. Merge Names        - https://www.testdome.com/questions/c-sharp/merge-names/27738?visibility=1&skillId=18
-3. Palindrome         - https://www.testdome.com/questions/c-sharp/palindrome/7282?visibility=1&skillId=18
-4. Quadratic Equation - https://www.testdome.com/questions/c-sharp/quadratic-equation/31592?visibility=1&skillId=18
-5. Binary Search Tree - https://www.testdome.com/questions/c-sharp/binary-search-tree/24999?visibility=1&skillId=18
-6. Song               - https://www.testdome.com/questions/c-sharp/song/25559?visibility=1&skillId=18
-7. User Input         - https://www.testdome.com/questions/c-sharp/user-input/11976?visibility=1&skillId=18
-8. Account Test       - https://www.testdome.com/questions/c-sharp/account-test/30738?visibility=1&skillId=18
-9. Two Sum            - https://www.testdome.com/questions/c-sharp/two-sum/24954?visibility=1&skillId=18
-10. Account           - https://www.testdome.com/questions/c-sharp/account/32673?visibility=1&skillId=18
-11. Sorted Search     - https://www.testdome.com/questions/c-sharp/sorted-search/35884?visibility=1&skillId=18
+1. Alert Service      - https://www.testdome.com/questions/c-sharp/alert-service/31293
+2. Merge Names        - https://www.testdome.com/questions/c-sharp/merge-names/27738
+3. Palindrome         - https://www.testdome.com/questions/c-sharp/palindrome/7282
+4. Quadratic Equation - https://www.testdome.com/questions/c-sharp/quadratic-equation/31592
+5. Binary Search Tree - https://www.testdome.com/questions/c-sharp/binary-search-tree/24999
+6. Song               - https://www.testdome.com/questions/c-sharp/song/25559
+7. User Input         - https://www.testdome.com/questions/c-sharp/user-input/11976
+8. Account Test       - https://www.testdome.com/questions/c-sharp/account-test/30738
+9. Two Sum            - https://www.testdome.com/questions/c-sharp/two-sum/24954
+10. Account           - https://www.testdome.com/questions/c-sharp/account/32673
+11. Sorted Search     - https://www.testdome.com/questions/c-sharp/sorted-search/35884
+12. Train Composition - https://www.testdome.com/questions/c-sharp/train-composition/9929
